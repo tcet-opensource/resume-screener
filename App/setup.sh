e2a0a70d02a6058e462f0b20e72b8a2a2f07f7f0
Hrishikesh332 <93283437+Hrishikesh332@users.noreply.github.com>
2023-03-31 22:48:43 +0530

    Add files via upload

---
+++ App/setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+
+[theme]
+base=\"dark"\n\" > ~/.streamlit/config.toml
