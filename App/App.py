e86e4855c9543c8c5ddceb36c5461b80b317b604
Prathik Shetty <63772910+prathikshetty2002@users.noreply.github.com>
2023-03-31 16:15:38 +0530

    Feature : File upload and display (pdf) (#4)
    * feat: upload & display resume(pdf only)
    
    * add: upload resumes folder & imgs folder

--- App/App.py
+++ App/App.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import pandas as pd
+import base64, random
+import time,datetime
+import os
+from pyresparser import ResumeParser
+from PIL import Image
+import nltk
+nltk.download('stopwords')
+
+
+st.set_page_config(
+   page_title="Resume Screener",
+   page_icon='./imgs/design.jpg',
+)
 
 def main():
     activities = ["User", "Admin"]
@@ -7,6 +21,28 @@ def main():
 
 def User():
     st.write('Welcome to user panel')
+    st.markdown('''<h5 style='text-align: left; color: #70FA79;'> Upload Your Resume </h5>''',unsafe_allow_html=True)
+    
+    # show uploaded file path to view pdf_display
+    def show_pdf(file_path):
+        with open(file_path, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
+        
+    ## file upload in pdf format
+    pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+    if pdf_file is not None:
+        with st.spinner('Hang On While We Cook Magic For You...'):
+            time.sleep(4)
+    
+    ### saving the uploaded resume to folder
+        save_image_path = './Uploaded_Resumes/'+pdf_file.name
+        pdf_name = pdf_file.name
+        with open(save_image_path, "wb") as f:
+            f.write(pdf_file.getbuffer())
+        show_pdf(save_image_path)
+
    
 def Admin():
     st.write('Welcome to admin panel')
@@ -15,5 +51,6 @@ if main() == "User":
     User()
 else:
     Admin()
-    
+
+  
 
